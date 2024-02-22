8117bf0adfd55f4340c6d060ddf32280c11507bf
William Laursen <Tlaursen8@yahoo.com>
2024-02-22 17:32:38 -0500

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const homeStreet = 42
+const distanceFromHqInBlocks = (passengerLocation) => {
+    return Math.abs(passengerLocation - homeStreet)
+}
+const distanceFromHqInFeet = (passengerLocation) => {
+    let distanceInBlocks = distanceFromHqInBlocks(passengerLocation)
+    let distanceInFeet = distanceInBlocks * 264
+    return distanceInFeet
+}
+const distanceTravelledInFeet = (startBlock, endBlock) => {
+    let blocks = Math.abs(startBlock - endBlock)
+    let blocksInFeet = blocks * 264
+    return blocksInFeet
+}
+const calculatesFarePrice = (start, destination) => {
+    let distance = distanceTravelledInFeet(start, destination)
+    if(distance < 400) {
+        return 0
+
+    } else if(distance >= 400 && 2000 >= distance) {
+        return (distance - 400) * .02
+    } else if (distance < 2500) {
+        return 25
+        } else {
+            return 'cannot travel that far'
+        }
+}
